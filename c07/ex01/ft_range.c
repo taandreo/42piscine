56f46d07a06d26e62f9b730cfd473744a9b8fc45
Tairan Andreo <tairan@hotmail.com>
2022-04-24 12:56:52 -0300

    adding files

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -6,15 +6,16 @@ int *ft_range(int min, int max)
 	int range;
 	int i;
 
+	i = 0;
 	if (min > max)
 		return (0);
 	if (max > 2147483647)
 		return (0);
-	range = max - min;
+	range = max - min + 1;
 	buffer = malloc(range * sizeof(int));
 	if (!buffer)
 		return (0);
-	while(i < range)
+	while(min <= max)
 	{
 		buffer[i] = min;
 		min++;
--- c07/ex01/main.c
+++ c07/ex01/main.c
@@ -4,11 +4,16 @@ int *ft_range(int min, int max);
 
 int main(void)
 {
+	int min = 4;
+	int max = 333;
+	int size;
+
+	size  = max - min + 1;
 	int i = 0;
 	int *range;
-	range = ft_range(3, 333);
+	range = ft_range(min, max);
 
-	while(i < 333 - 3)
+	while(i < size)
 	{
 		printf("%i, ", range[i]);
 		i++;
